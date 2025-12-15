8bde81ca7162e195e5b324946d052c5d57d7d750
Rohan Bendapudi <datanteater@gmail.com>
2025-12-15 15:15:24 -0500

    updated trie.h to include all the functions

--- src/trie.h
+++ src/trie.h
@@ -1,9 +1,10 @@
 #pragma once
-
+# include <stdlib.h>
+# include <stdint.h>
 #define CHILDREN_SIZE 256
 
 struct TrieNode {
-    unsigned char byte;
+    u_int8_t byte;
     short int phrase_number; 
     struct TrieNode *parent;
     struct TrieNode *children[CHILDREN_SIZE];
@@ -24,3 +25,9 @@ Node* create_node(unsigned char character, Node* parent_node, short int phrase_n
 void insert_node(unsigned char byte, Node* node, int phrase_number);
 
 void search_trie(unsigned char* byte_stream, Node* root, Result* search_result, int string_size);
+
+void image_compression(unsigned char* image_data, Node* root_node, size_t image_size);
+
+void free_trie(Node* node);
+
+int create_trie();
