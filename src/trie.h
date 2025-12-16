cf41cf9aa8d8c3decd4ae1e1beeefc50ee470b03
Rohan Bendapudi <datanteater@gmail.com>
2025-12-16 01:58:01 -0500

    made some small updates to method signatures and removed the STB_IMAGE_IMPLEMENTATION define line from the trie.c file, since I defined that in main.c     --> was struggling for this for a while as it was causing a segfault as i defined STB_IMAGE_IMPLEMENTATION in trie.c but not main.c

--- src/trie.h
+++ src/trie.h
@@ -1,6 +1,8 @@
 #pragma once
-# include <stdlib.h>
 # include <stdint.h>
+# include <stdlib.h>
+
+
 #define CHILDREN_SIZE 256
 
 struct TrieNode {
@@ -26,8 +28,9 @@ void insert_node(unsigned char byte, Node* node, int phrase_number);
 
 void search_trie(unsigned char* byte_stream, Node* root, Result* search_result, int string_size);
 
-void image_compression(unsigned char* image_data, Node* root_node, size_t image_size);
+int image_compression(unsigned char* image_data, Node* root_node, size_t image_size);
 
 void free_trie(Node* node);
 
 int create_trie();
+
